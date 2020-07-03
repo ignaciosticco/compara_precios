3ae663026bac838ac692e53f8a28654c5d46d7cc
Your Name <ignaciosticco@gmail.com>
2020-07-02 22:45:14 -0300

    logre acceder a todos los elementos dentro de top

--- obtiene_precios.py
+++ obtiene_precios.py
@@ -5,20 +5,41 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
-
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-driver.get("https://supermercado.carrefour.com.ar")
-
-id_busqueda = driver.find_element_by_id('search')
-id_busqueda.send_keys('coca cola')
-id_busqueda.send_keys(Keys.ENTER)
 
 
-titles = driver.find_elements_by_xpath('//div[@class="producto-info"]')
-#titles = driver.find_elements_by_xpath('//div[@class="home-product-cards container_tg"]')
-#titles = driver.find_elements_by_xpath('//div[@class="col s12 product-card product-card-food"]')
+def accede_al_sitio(url):
 
-print(titles)   
+	driver.get("{}".format(url))
 
 
+def busca_producto(producto):
+
+	id_busqueda = driver.find_element_by_id('search')
+	id_busqueda.send_keys('{}'.format(producto))
+	id_busqueda.send_keys(Keys.ENTER)
+
+def muestra_elementos():
+	'''
+	Imprime todos los elementos del sitio
+	'''
+
+	ids = driver.find_elements_by_xpath('//*[@id]') 
+	for i in ids:
+		print(i.tag_name)
+		print(i.get_attribute('id'))    
+
+def main():
+
+	url = 'https://supermercado.carrefour.com.ar'
+	accede_al_sitio(url)
+	producto = 'coca cola'
+	busca_producto(producto)
+	#muestra_elementos()
+
+	lista = driver.find_element_by_id('top').find_elements_by_tag_name('div')
+	print(lista)
+	print(lista[0].get_attribute('id'))
+
+if __name__=='__main__':
+     main()
