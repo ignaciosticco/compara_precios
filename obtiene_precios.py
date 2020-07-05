aaad7aa36c61ce0f21d5eae99c98dd41c912b8bf
Your Name <ignaciosticco@gmail.com>
2020-07-05 18:44:46 -0300

    logre que obtenga una lista de precios y productos para Carrefour. Falta testearlo

--- obtiene_precios.py
+++ obtiene_precios.py
@@ -48,9 +48,9 @@ def nombra_todo():
 
 	ids = driver.find_elements_by_xpath('//*[@id]') 
 	for i in ids:
-		print("ID: ",i.get_attribute('id'))
-		print("Texto: ",i.text)	
-		print(" ")
+		print("ID: {}\n".format(i.get_attribute('id')))
+		print("Texto: {}\n".format(i.text))	
+
 
 def devuelve_lista_precios():
 
@@ -59,18 +59,58 @@ def devuelve_lista_precios():
 		if 'product-price' in i.get_attribute('id') and 'amlabel' not in i.get_attribute('id') :
 			print("Precio = ",i.text)  
 
+
+def extrae_producto_y_precio(texto):
+
+	'''
+	Le pasas el texto donde estan todos los precios y extrae el nombre de los productos y sus precios. 
+	Warning: Esta funcion solo sirve para Carrefour 
+	'''
+
+	lista_texto_dividido = texto.split("\n")	
+	lista_texto_dividido = [x for x in lista_texto_dividido if x] # saco los elementos vacios
+	producto_precio = []
+	for i in range(0,len(lista_texto_dividido)):
+		if lista_texto_dividido[i][0]=='$':
+			if "Por unidad" in lista_texto_dividido[i+1]:
+				producto = lista_texto_dividido[i+4]
+				precio = lista_texto_dividido[i]
+				producto_precio+=[[producto,precio]]
+			else:
+				producto = lista_texto_dividido[i+2]
+				precio = lista_texto_dividido[i]
+				producto_precio+=[[producto,precio]]
+
+	return producto_precio
+
+def imprime_lista_precios(producto_precio):
+	'''
+	Imprime en la terminal una tabla de productos y precios
+	'''
+
+	print("\nProducto\t\t\tPrecio ")
+	for x in producto_precio:
+		print("{}\t\t{}".format(x[0],x[1]))
+
+
+
+
 def main():
 
 	url = 'https://supermercado.carrefour.com.ar'
 	accede_al_sitio(url)
-	producto = 'coca cola'
+	#producto = 'coca cola'
+	print("¿Qué producto buscas?\n")
+	producto = str(input())
 	busca_producto(producto)
-	#muestra_elementos()
-	#nombra_todo()
-	#lista = driver.find_element_by_id('top').find_elements_by_tag_name('div')
 	
 	time.sleep(2)	# Espera 2 seg antes de buscar los precios 
-	devuelve_lista_precios()
+	ids = driver.find_elements_by_xpath('//*[@id]') 
+	texto_data_productos = ids[0].text # Estoy asumiendo que estan en el primer elemento de la lista de elementos
+	producto_precio = extrae_producto_y_precio(texto_data_productos)
+	imprime_lista_precios(producto_precio)
+	
+
 
 
 if __name__=='__main__':
