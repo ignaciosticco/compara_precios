8e570e666646d5c49d1fb6692ea59197f93b3c87
Your Name <ignaciosticco@gmail.com>
2020-07-08 22:41:28 -0300

    pude acercarme al elemento del precio pero todavia no lo puedo leer

--- obtiene_precios_coto.py
+++ obtiene_precios_coto.py
@@ -2,12 +2,15 @@ import bs4 as bs
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from requests import get
 
 
 class precioBot_coto:
 
 	def __init__(self):
 		self.driver = webdriver.Chrome()
+
 
 	def accede_al_sitio(self, url):
 		self.driver.get("{}".format(url))
@@ -22,12 +25,27 @@ class precioBot_coto:
 
 def main():
 
+	
+
 	bot = precioBot_coto()
 	url = 'http://www.cotodigital3.com.ar/'
 	bot.accede_al_sitio(url)
 	print("¿Qué producto buscas?\n")
 	producto = str(input())
 	bot.busca_producto(producto)
+
+	response = get(bot.driver.current_url)
+	html_soup = BeautifulSoup(response.text, 'html.parser')
+
+
+	product_containers = html_soup.find_all('li', class_ = 'clearfix')
+	producto = product_containers[0]
+	test1 = producto.span.span
+	#print(test1)
+
+
+	time.sleep(10)
+	
 
 if __name__ == '__main__':
     main()
