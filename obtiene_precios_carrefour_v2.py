a78539bd9363e0d0e28cbfa7a32023c04dcc62f6
Your Name <ignaciosticco@gmail.com>
2020-08-30 15:20:32 -0300

    primer intento de hosteo en heroku

--- obtiene_precios_carrefour_v2.py
+++ obtiene_precios_carrefour_v2.py
@@ -3,11 +3,20 @@ Este script encuentra e imprime los precios del supermercado carrefour
 Hay que pasarle un url del producto cuyo precio se quiere imprimir
 '''
 
-
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+
+from selenium import webdriver
+import os
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 class PrecioBot:
 
