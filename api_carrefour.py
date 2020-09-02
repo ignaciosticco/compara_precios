26185bf29b2d3127d7b77d4d132da31510970fc2
Your Name <ignaciosticco@gmail.com>
2020-09-02 19:26:21 -0300

    movi lo de hello.py a api_carrefour.py

--- api_carrefour.py
+++ api_carrefour.py
@@ -2,7 +2,6 @@ from time import sleep
 from flask import Flask, jsonify
 from obtiene_precios_carrefour_v2 import PrecioBot
 import time
-'''
 from selenium import webdriver
 import os
 
@@ -12,18 +11,15 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-'''
+
 app = Flask(__name__)
 
-
-
+###################### DATA DE PRODUCTOS ######################
 product_id = [1,2]
 url1 = 'https://supermercado.carrefour.com.ar/lacteos-y-productos-frescos/leches/leche-entera-larga-vida-la-serenisima-3-1-l.html'
 url2 = 'https://supermercado.carrefour.com.ar/bebidas/gaseosa-coca-cola-light-2-5-l.html'
 lista_urls = [url1,url2]
 lista_nombre_productos = ['Leche entera','Coca Cola']
-
-
 
 
 @app.route('/')
@@ -36,7 +32,8 @@ def main():
 	lista_precio_out = []
 
 	bot = PrecioBot()
-	string_out = 'Precios de Carrefour\n\n'
+	string_out = 'Compara Precios de supermercados \n\nPrecios de Carrefour\n\n'
+	'''
 	for i in range (0,len(lista_urls)):
 		bot.accede_al_sitio(lista_urls[i])
 		time.sleep(4)
@@ -47,20 +44,17 @@ def main():
 		string_out += "\nProducto: {}\nPrecio: ${}\n\n".format(lista_nombre_productos[i],precio_out)
 		print(string_out)
 		string_out+="\n\nUltima actualizacion: {}".format(time.strftime("%d-%m-%y %H:%M:%S", time.gmtime()))
+	'''
 	return string_out
 
-
+'''
 @app.route('/json')
 def solapa_json():
-	'''
-	En la pagina /json muestra el diccionario de productos y precios 
-	'''
+
+	#En la pagina /json muestra el diccionario de productos y precios 
 
 	diccionario_out = {'nombre_producto':lista_nombre_productos,
-						'precio_producto':lista_precio_out}
-	
+	'precio_producto':lista_precio_out}
+
 	return jsonify(diccionario_out)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+'''
