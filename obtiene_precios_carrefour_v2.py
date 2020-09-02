88d9ec2acb70d9f9e831c50de456c468038ba84d
Your Name <ignaciosticco@gmail.com>
2020-09-02 20:46:57 -0300

    puse las opciones de chrome afuera de la clase

--- obtiene_precios_carrefour_v2.py
+++ obtiene_precios_carrefour_v2.py
@@ -9,15 +9,17 @@ import time
 import os
 
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+
+
 class PrecioBot:
-	'''
-	chrome_options = webdriver.ChromeOptions()
-	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-	chrome_options.add_argument("--headless")
-	chrome_options.add_argument("--disable-dev-shm-usage")
-	chrome_options.add_argument("--no-sandbox")
-	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-	'''
+
 
 	def __init__(self):
 		self.driver = webdriver.Chrome()
