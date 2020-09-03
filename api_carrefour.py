c9561543ede84b26ecb0c9a3b79cd5bee47fa77e
Your Name <ignaciosticco@gmail.com>
2020-09-02 21:56:53 -0300

    comente cosas que no se si suman

--- api_carrefour.py
+++ api_carrefour.py
@@ -32,7 +32,6 @@ def main():
 	lista_precio_out = []
 
 	bot = PrecioBot()
-	#driver = webdriver.Chrome()
 	string_out = 'Compara Precios de supermercados \n\nPrecios de Carrefour\n\n'
 
 	for i in range (0,len(lista_urls)):
