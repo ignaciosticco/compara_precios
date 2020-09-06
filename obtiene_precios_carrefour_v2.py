a9fe61be70dc6d43e80c1972226c17ddeb1aa4cd
Your Name <ignaciosticco@gmail.com>
2020-09-06 19:32:22 -0300

    testeos

--- obtiene_precios_carrefour_v2.py
+++ obtiene_precios_carrefour_v2.py
@@ -20,7 +20,7 @@ class PrecioBot:
 		#chrome_options.add_argument("--disable-dev-shm-usage")
 		#chrome_options.add_argument("--no-sandbox")
 		#driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
+		print("hola")
 		#self.driver = webdriver.Chrome()
 
 	def accede_al_sitio(self, url):
