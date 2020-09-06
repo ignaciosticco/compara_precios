befafdfc65e406d58454e1bc427711a9a4dad8ec
Your Name <ignaciosticco@gmail.com>
2020-09-06 20:52:27 -0300

    prueba minimalista

--- api_carrefour.py
+++ api_carrefour.py
@@ -10,7 +10,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage") 
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--example-flag")
+
    
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
@@ -30,15 +30,17 @@ def main():
 	En la landing page imprime los nombres y precios de los productos
 	'''
 
-	global lista_precio_out
-	lista_precio_out = []
+	#global lista_precio_out
+	#lista_precio_out = []
 	webdriver.Chrome()
 
+	return 
+	'''
 	#bot = PrecioBot()
 	string_out = 'Compara Precios de supermercados \n\nPrecios de Carrefour\n\n'
 
 	for i in range (0,len(lista_urls)):
-		'''
+		
 		bot.accede_al_sitio(lista_urls[i])
 		time.sleep(4)
 		precio = bot.driver.find_element_by_class_name("regular-price") 
@@ -48,10 +50,10 @@ def main():
 		string_out += "\nProducto: {}\nPrecio: ${}\n\n".format(lista_nombre_productos[i],precio_out)
 		print(string_out)
 		string_out+="\n\nUltima actualizacion: {}".format(time.strftime("%d-%m-%y %H:%M:%S", time.gmtime()))
-		'''
+		
 	return string_out
 	#webDriver.Dispose()
-
+	'''
 '''
 @app.route('/json')
 def solapa_json():
