1b411b8b7b8c01ed108dbceaa3a70a6b714a4150
Your Name <ignaciosticco@gmail.com>
2020-09-13 00:12:46 -0300

    invoco sched desde main

--- api_carrefour.py
+++ api_carrefour.py
@@ -24,21 +24,19 @@ lista_urls = [url1,url2]
 lista_nombre_productos = ['Leche entera','Coca Cola']
 
 
-'''
 app = Flask(__name__)
 @app.route('/')
 def main():
-	return "string_out"
-'''
+	string = timed_job()
+	return string
+
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every three minutes.')
+    return "aas"
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
 
 sched.start()
 
