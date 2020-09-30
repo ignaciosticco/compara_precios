e50a9be6dcd0fe4efebc438be0ea9e36a8a987bc
Your Name <ignaciosticco@gmail.com>
2020-09-30 19:05:50 -0300

    simplifique el script de sched

--- sched.py
+++ sched.py
@@ -1,10 +1,4 @@
-from time import sleep
-from flask import Flask, jsonify
-from obtiene_precios_carrefour_v2 import PrecioBot
 import time
-from selenium import webdriver
-import os
-from apscheduler.schedulers.blocking import BlockingScheduler
 import schedule
 
 def job():
