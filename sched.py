35bfff149373be537cd8c38f070c7d59458053aa
Your Name <ignaciosticco@gmail.com>
2020-10-12 18:01:14 -0300

    hago pruebas con la base de datos de Postgress

--- sched.py
+++ sched.py
@@ -1,6 +1,38 @@
 import time
 import schedule
+import psycopg2
+import psycopg2.extras
 
+DB_HOST = "ec2-34-235-62-201.compute-1.amazonaws.com"
+DB_NAME = "de5j3eiug6jr8t"
+DB_USER = "lonhfypadrcwga"
+DB_PASS = "47db36530ea1899a22c36daae1b3eadce120520c46e7b92a24eaf9144a2b1f22"
+
+
+conn = psycopg2.connect(dbname=DB_NAME,user=DB_USER,password=DB_PASS,host=DB_HOST)
+
+with conn:
+	with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
+		cur.execute("SELECT * FROM productos;")
+		print(cur.fetchall())
+
+conn.close()
+
+'''
+conn = psycopg2.connect(dbname=DB_NAME,user=DB_USER,password=DB_PASS,host=DB_HOST)
+cur = conn.cursor()
+#cur.execute("CREATE TABLE productos (id SERIAL PRIMARY KEY, name VARCHAR);")
+
+cur.execute("INSERT INTO productos (name) VALUES(%s)",("Coca Cola",))
+
+conn.commit()
+cur.close()
+conn.close()
+'''
+
+
+'''
 def job():
 	print("Funciona")
 	string_out = "\n\nUltima actualizacion: {}".format(time.strftime("%d-%m-%y %H:%M:%S", time.gmtime()))
@@ -13,3 +45,4 @@ schedule.every(1).minutes.do(job)
 while True:
     schedule.run_pending()
     time.sleep(1) # wait one minute
+'''
